46597056db5050787146da83d73793e1bd15b447
standarthoid <90977509+standarthoid@users.noreply.github.com>
2022-05-27 13:02:35 +0200

    setup functions to show forecast

--- src/script.js
+++ src/script.js
@@ -28,7 +28,8 @@ function formatDay(timestamp) {
   return days[day];
 }
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
   let days = ["Fri", "Sat", "Sun", "Mon", "Tue"];
@@ -38,7 +39,8 @@ function displayForecast() {
       `<div class ="col-2">
           <div class="card2" style="width: 13rem;">
   <div class="card-body Saturday">
-    <h5 class="card-title">${day} <br/>
+    <h5 class="card-title">${day}
+     <br/>
       <br/>🌤
     </h5>
     <br/>
@@ -50,8 +52,13 @@ function displayForecast() {
   forecastElement.innerHTML = forecastHTML;
 }
 
-displayForecast();
-
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "b68955b53e53008eb9772143fe4d18a1";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&unit=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
+}
 function weatherConditions(response) {
   celsiusTemperature = response.data.main.temp;
   document.querySelector("#current-city").innerHTML = `${response.data.name}`;
@@ -79,6 +86,7 @@ function weatherConditions(response) {
   document
     .querySelector("#icon")
     .setAttribute("alt", response.data.weather[0].description);
+  getForecast(response.data.coord);
 }
 
 function search(city) {
